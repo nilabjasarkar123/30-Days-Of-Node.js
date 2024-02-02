1a9df473d34bfa3d67f102b2658e39936c13f752
nilabjasarkar123 <nil.sarkar002@gmail.com>
2024-02-02 22:03:22 +0530

    question 2

---
+++ Question2.js
@@ -0,0 +1,18 @@
+function writeToFile(filePath, content) {
+    const fs = require('node:fs');
+    
+    fs.writeFile(filePath, content, err => {
+        if (err) {
+            console.error(err);
+        } 
+        else {
+            console.log(content);
+        }
+    });
+}
+
+writeToFile('test-files/output1.txt', 'Sample content.');
+// Expected Output: Data written to output1.txt
+
+writeToFile('test-files/nonexistent-folder/output.txt', 'Content in a non-existent folder.');
+// Expected Output: Error writing to file: ENOENT: no such file or directory...
