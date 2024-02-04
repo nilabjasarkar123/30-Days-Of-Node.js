a1e7a08b3c64769c97e7456b6aca7b8b8d66b28b
nilabjasarkar123 <nil.sarkar002@gmail.com>
2024-02-04 21:50:13 +0530

    Question 4

---
+++ Question4.js
@@ -0,0 +1,12 @@
+function resolvePath(relativePath) {
+    // Implementation
+    const path = require('node:path'); 
+    path1 = path.resolve(relativePath);
+    console.log("Resolved Path: ",path1)
+  
+}
+resolvePath('../project/folder/file.txt');
+// Expected Output: Resolved Path: /Users/username/project/folder/file.txt
+
+// resolvePath('nonexistent-folder/file.txt');
+// Expected Output: Resolved Path: /Users/username/nonexistent-folder/file.txt
